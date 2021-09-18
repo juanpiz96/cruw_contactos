27cab57d3087d188e5cb2550e71476855ad9223b
Juan <jupagaso@gmail.com>
2021-09-17 19:50:02 -0500

    iniciando repositorio

--- cruw_contactos.py
+++ cruw_contactos.py
@@ -1,1 +1,28 @@
-print("hola mundo")
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='127.0.0.1',
+    user='juan',
+    password='123456',
+    database='blog_cuarta_generacion',
+    port=3308
+
+)
+def crearUsuario(nombre, email, contrasena):
+    cursor = db.cursor()
+
+    cursor.execute('''insert into
+    usuarios(nombre, email, contrasena)
+    values(%s, %s, %s)''',(
+        nombre,
+        email, 
+        contrasena
+    ))
+    #creacion, modificacion, eliminacion de datos para
+    db.commit()
+    cursor.close() 
+crearUsuario('juanpa','jupa@gmail.com','1256')
+cursor = db.cursor()
+cursor.execute('select * from usuarios')
+usuarios = cursor.fetchall()
+print(usuarios)
